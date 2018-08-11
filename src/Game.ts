61b43398767f779d2637a96e94ed8af37b5338b9
KvisazGames <kiko@kiko.kiko>
2018-08-11 21:12:07 +0700

    states folders and README update

--- src/Game.ts
+++ src/Game.ts
@@ -6,8 +6,8 @@ module MyGame {
 
 			super(800, 600, Phaser.AUTO, 'content', null);
 
-			this.state.add('Boot', Boot, false);
-			this.state.add('Preloader', Preloader, false);
+			this.state.add('Boot', Boot, false); // настройка игры и загрузка ассетов для прелоадера
+			this.state.add('Preloader', Preloader, false); // основной экран с прогрессбаром для загрузки
 			this.state.add('MainMenu', MainMenu, false);
 			this.state.add('Level1', Level1, false);
 
